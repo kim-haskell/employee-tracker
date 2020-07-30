c640653237f6055c6e6272aa25f76b4448acac6b
Kim Haskell <kim.haskell03@gmail.com>
2020-07-30 17:01:29 -0600

    adding basic files and npm packages

---
+++ employees.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "kjsmalley7634",
+    database: ""
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
